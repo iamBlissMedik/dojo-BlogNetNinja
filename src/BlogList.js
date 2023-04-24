425618f31ded5badfd56e238b54610f5fafc2c3b
Ellams Bliss <ellamsbliss@gmail.com>
2023-04-25 01:01:43 +1000

    learning how to use custom hooks

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,13 +1,12 @@
-const BlogList = ({ blogs, title,handleDelete }) => {
-
+const BlogList = ({ blogs, title }) => {
   return (
     <div className="blog-list">
       <h1>{title}</h1>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
-              <p>Written by {blog.author}</p>
-              <button onClick={()=>handleDelete(blog.id)}>delete blog</button>
+          <p>Written by {blog.author}</p>
+         
         </div>
       ))}
     </div>
--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,14 @@
-import { useState,useEffect } from "react";
+import useFetch from "./useFetch";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-    };
-    useEffect(() => {
-        console.log(`use effect ran`)
-    })
+  const { data, isPending, error } = useFetch("http://localhost:8000/blogs");
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      {error && <div>{error}</div>}
+      {isPending && <div>loading...</div>}
+      {data && <BlogList blogs={data} title="All Blogs" />}
     </div>
   );
 };
